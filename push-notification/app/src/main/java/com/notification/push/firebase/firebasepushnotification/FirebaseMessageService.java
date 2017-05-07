ccd6a83b991b8d10b9dd8d7aeb3ac4453d283c26
marabesi <matheus.marabesi@gmail.com>
2017-05-07 17:18:30 -0300

    run on main thread(ui) fix

--- push-notification/app/src/main/java/com/notification/push/firebase/firebasepushnotification/FirebaseMessageService.java
+++ push-notification/app/src/main/java/com/notification/push/firebase/firebasepushnotification/FirebaseMessageService.java
@@ -1,17 +1,25 @@
 package com.notification.push.firebase.firebasepushnotification;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class FirebaseMessageService extends FirebaseMessagingService {
 
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
+    public void onMessageReceived(final RemoteMessage remoteMessage) {
         Log.d(FirebaseTokenService.TAG, "From: " + remoteMessage.getFrom());
 
-        Toast.makeText(getApplicationContext(), remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT);
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        handler.post(new Runnable() {
+            public void run() {
+                Toast.makeText(getApplicationContext(), remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
